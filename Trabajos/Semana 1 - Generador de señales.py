4186be87e178f542e957ad19610d3a7e4cb5e6bf
Santiago-Corral <santiagomcorral@gmail.com>
2025-03-12 17:47:19 -0300

    Arme la entrega del TS1

--- Trabajos/Semana 1 - Generador de señales.py
+++ Trabajos/Semana 1 - Generador de señales.py
@@ -26,16 +26,54 @@ def mi_funcion_square (vmax, dc, ff, ph, nn, fs):
 # Datos generales de la simulación
 fs = 1000 # frecuencia de muestreo (Hz)
 N = 1000  # cantidad de muestras
+
+#Distintas simulaciónes variando f0
+f0 = 1 #tiempo (Hz)  
+tt1, xx1 = mi_funcion_sen( vmax = 1, dc = 0, ff = f0, ph=0, nn = N, fs = fs)
+f0 = 10 #tiempo (Hz)  
+tt6, xx6 = mi_funcion_sen( vmax = 1, dc = 0, ff = f0, ph=0, nn = N, fs = fs)
+f0 = 500 #tiempo (Hz)  
+tt2, xx2 = mi_funcion_sen( vmax = 1, dc = 0, ff = f0, ph=0, nn = N, fs = fs)
+f0 = 999 #tiempo (Hz)  
+tt3, xx3 = mi_funcion_sen( vmax = 1, dc = 2, ff = f0, ph=0, nn = N, fs = fs)
+f0 = 1001 #tiempo (Hz)  
+tt4, xx4 = mi_funcion_sen( vmax = 1, dc = 4, ff = f0, ph=0, nn = N, fs = fs)
 f0 = 2001 #tiempo (Hz)  
+tt5, xx5 = mi_funcion_sen( vmax = 1, dc = 4, ff = f0, ph=0, nn = N, fs = fs)
 
+#Ploteo las simulaciones
 plt.figure()
-
-tt, xx = mi_funcion_sen( vmax = 2, dc =0, ff = f0, ph=0, nn = N, fs = fs)
-tt, xx2 = mi_funcion_square( vmax = 2, dc =0, ff = f0, ph=0, nn = N, fs = fs)
-
-plt.plot(tt,xx)
-plt.plot(tt,xx2)
+plt.plot(tt1,xx1,label="ff = 1Hz, dc = 0")
+plt.plot(tt2,xx2,label="ff = 500Hz, dc = 0")
+plt.plot(tt3,xx3,label="ff = 999Hz, dc = 2")
+plt.plot(tt4,xx4,label="ff = 1001Hz, dc = 4")
+plt.plot(tt5,xx5,label="ff = 2001Hz, dc = 4")
+plt.plot(tt6,xx6,label="ff = 10Hz, dc = 0")
+plt.title("Señal: ")
+plt.xlabel("tiempo [segundos]")
+plt.ylabel("Amplitud")
+plt.legend()
+plt.show()
 plt.grid 
+
+#Distintas simulaciónes variando f0
+f0 = 1 #tiempo (Hz)  
+tt1, xx1 = mi_funcion_square( vmax = 1, dc = 0, ff = f0, ph=0, nn = N, fs = fs)
+f0 = 999 #tiempo (Hz)  
+tt3, xx3 = mi_funcion_square( vmax = 1, dc = 5, ff = f0, ph=0, nn = N, fs = fs)
+f0 = 1001 #tiempo (Hz)  
+tt4, xx4 = mi_funcion_square( vmax = 1, dc = -5, ff = f0, ph=0, nn = N, fs = fs)
+
+#Ploteo las simulaciones
+plt.figure()
+plt.plot(tt1,xx1,label="ff = 1Hz, dc = 0") #Genera un primer valor de 0!!
+plt.plot(tt3,xx3,label="ff = 999Hz, dc = 5")
+plt.plot(tt4,xx4,label="ff = 1001Hz, dc = -5")
+plt.title("Señal: ")
+plt.xlabel("tiempo [segundos]")
+plt.ylabel("Amplitud")
+plt.legend()
+plt.show()
 
 """def generador_senal(tipo="senoidal", vmax=1, dc=0, ff=10, ph=0, nn=1000, fs=1000):
     ts = 1 / fs  # Tiempo de muestreo
